fc7ec472ddc0e88cef015f31842dd0c7f2b7164e
jakob <jakobsereda@gmail.com>
2024-06-27 22:38:55 -0600

    feat: added font sprites and reset method

--- src/core.rs
+++ src/core.rs
@@ -8,6 +8,27 @@ const NUM_KEYS: usize = 16;
 
 const START_ADDRESS: u16 = 0x200;
 
+const FONTSET_SIZE: usize = 80;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+];
+
 // -- CHIP-8 CPU Specification --
 pub struct CPU {
     // -- memory --
@@ -42,9 +63,8 @@ pub struct CPU {
 }
 
 impl CPU {
-    // -- constructor --
     pub fn new() -> Self {
-        Self {
+        let mut cpu = Self {
             ram: [0; RAM_SIZE],
             v_reg: [0; NUM_REGS],
             i_reg: 0,
@@ -55,9 +75,26 @@ impl CPU {
             stack: [0; STACK_SIZE],
             keys: [false; NUM_KEYS],
             display: [false; DISPLAY_WIDTH * DISPLAY_HEIGHT]
-        }
+        };
+
+        cpu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        cpu
     }
 
+    pub fn reset(&mut self) {
+        self.ram = [0; RAM_SIZE];
+        self.v_reg = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.dt = 0;
+        self.st = 0;
+        self.pc = START_ADDRESS;
+        self.sp = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.display = [false; DISPLAY_WIDTH * DISPLAY_HEIGHT];
+        self.ram [..FONTSET_SIZE].copy_from_slice(&FONTSET);
+    }
 
     fn push(&mut self, val: u16) {
         self.stack[self.sp as usize] = val;
