9963804b4b5bc46264a6ba29574e2f52cd0c9dfd
jakob <jakobsereda@gmail.com>
2024-07-04 16:48:28 -0600

    feat: added some SDL boilerplate

--- src/main.rs
+++ src/main.rs
@@ -11,4 +11,17 @@ fn main() {
         println!("Try running: cargo run path/to/game");
         return;
     }
+
+    let context = sdl2::init().unwrap();
+    let video_subsystem = context.video().unwrap();
+    let window = video_subsystem
+        .window("WEISBECKER", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+    
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    canvas.clear();
+    canvas.present();
 }
