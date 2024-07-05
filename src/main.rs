b9bf6ae01cc2500981ef122b5f1ec4dd9bdea451
jakob <jakobsereda@gmail.com>
2024-07-05 10:57:58 -0600

    feat(frontend): added running loop, the window is now alive...

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use core::*;
 use std::env;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 
-const SCALE: u32 = 15;
+const SCALE: u32 = 20;
 const WINDOW_WIDTH: u32 = (DISPLAY_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (DISPLAY_HEIGHT as u32) * SCALE;
 
@@ -12,8 +14,8 @@ fn main() {
         return;
     }
 
-    let context = sdl2::init().unwrap();
-    let video_subsystem = context.video().unwrap();
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
         .window("WEISBECKER", WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
@@ -24,4 +26,20 @@ fn main() {
     let mut canvas = window.into_canvas().present_vsync().build().unwrap();
     canvas.clear();
     canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit{ .. } | Event::KeyDown { 
+                    keycode: Some(Keycode::Escape), 
+                    .. 
+                } => break 'running,
+                _ => ()
+            }
+        }
+    }
 }
